4ae569169fded739928b5e4f90fbb1b2b2b67fb6
Sidnei Junior <135073086+Sidnei-Junior-dev@users.noreply.github.com>
2024-03-18 22:25:14 -0300

    Condição Simples, Comp.

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel= 60.5;
+console.log(`A velocidade do seu carro é de ${vel}Km/h.`);
+if(vel>60){//Condição Simples
+    console.log(`Ultrapassou a velocidade permitida, MULTADO!`);
+}
+console.log(`Dirija sempre com segurança!`);
