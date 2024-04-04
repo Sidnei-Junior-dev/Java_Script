1184c85ff35c318ca698519998e7b37f72a80e7d
Sidnei Junior <135073086+Sidnei-Junior-dev@users.noreply.github.com>
2024-04-03 22:59:14 -0300

    Function, Parâmetros

---
+++ aula17/funcao01.js
@@ -0,0 +1,9 @@
+function parimp(n){
+    if(n % 2 == 0){
+        return 'PAR!'
+    }else{
+        return 'IMPAR!'
+    }
+}
+let res= parimp(4);
+console.log(res);
---
+++ aula17/funcao02.js
@@ -0,0 +1,9 @@
+/*function soma(n1=0, n2=0){
+    return n1 + n2;
+}
+console.log(soma(5))*/
+
+var x= function(n){
+    return  n*2; 
+}
+console.log(x(2))
